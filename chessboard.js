b56fa24ed5722b833174665414b4777ffbd10699
Abdullah527382 <48465800+Abdullah527382@users.noreply.github.com>
2020-02-14 17:27:07 +1100

    Chessboard.js
    Creates a chessboard grid pattern of variable length that is predefined, can be changed in the text editor

---
+++ chessboard.js
@@ -0,0 +1,16 @@
+// Prints a chessboard of variable size defined by user
+// 14/02/2020, created by Abdullah Ahmed 
+
+let y = 0, x = 0, even = " #", odd = "# ";
+var size = 10;
+while (y < size){
+	// Every even row 
+	if (y % 2 == 0){
+		console.log(even.repeat(size/2));
+	// Every odd row 
+	} else {
+		console.log(odd.repeat(size/2));
+	// Every odd column 	
+	}
+	y++;
+}
